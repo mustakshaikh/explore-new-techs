69cdc10b74e10028637b167f9eebe174d648e5a8
mustakshaikh <mushtaqtheone@outlook.com>
2021-12-26 19:27:16 -0600

    Update SearchTree.java

--- SearchTree.java
+++ SearchTree.java
@@ -23,6 +23,11 @@ class BST
         Node newnode=new Node(key);
         Node currnode=treeroot;
         Node prevnode=null;
+        if(treeroot==null)
+        {
+            treeroot=newnode;
+            return;
+        }
         while(currnode != null)
         {
             if(currnode.key < newnode.key)
@@ -38,17 +43,44 @@ class BST
             }
             
         }
+        if(prevnode==null)
+        {
+            prevnode=newnode;
+            return;
+        }
+        
         if(prevnode.key < newnode.key)
             prevnode.right=newnode;
         else
             prevnode.left=newnode;
         
     }
+    
+    public void Display(Node node,String side)
+    {
+        if(node==null)
+            return;
+        else
+            {
+                System.out.println("Node Key=" + node.key + " on the " + side);
+            }
+       // if(node.left !=null)
+            Display(node.left,"left");
+        //if(node.right !=null)
+            Display(node.right,"right");
+    }
 }
 public class SearchTree {
     public static void main(String args[]) {
-        
-        
+        BST bst=new BST();
+        bst.Insert(50);
+        bst.Insert(30);
+        bst.Insert(20);
+        bst.Insert(40);
+        bst.Insert(70);
+        bst.Insert(60);
+        bst.Insert(80);
+        bst.Display(bst.treeroot,"");
     }
     }
     
